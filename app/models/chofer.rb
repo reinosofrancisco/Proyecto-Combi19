f677ef1b4319bd8e8ac083a91f7940fd01d8fe62
Reinoso Francisco <reinosofrancisco_@hotmail.com>
2020-11-07 17:37:30 -0300

    Chofers pa jero

---
+++ app/models/chofer.rb
@@ -0,0 +1,6 @@
+class Chofer < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
