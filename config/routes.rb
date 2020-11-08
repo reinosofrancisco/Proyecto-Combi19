1802ef040ed1e6e2867b924f2d04912ab458e87e
Reinoso Francisco <reinosofrancisco_@hotmail.com>
2020-11-08 14:48:26 -0300

    Cambios en el / y autenticacion de usuarios

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,25 @@
 Rails.application.routes.draw do
+  devise_for :admins
   devise_for :chofers
   devise_for :users
+
+
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
+  #Cambia la ruta por defecto a la ruta de "Log_IN" de la gema Devise
+  devise_scope :user do
+
+    #Cambia esto a algo que no sea ADMIN porque... ya sabes
+    authenticated :user do
+      root 'pages#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new'
+    end
+
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_07_205133) do
+ActiveRecord::Schema.define(version: 2020_11_08_164853) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
 
   create_table "chofers", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,6 +38,15 @@ ActiveRecord::Schema.define(version: 2020_11_07_205133) do
     t.string "telefono"
     t.index ["email"], name: "index_chofers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_chofers_on_reset_password_token", unique: true
+  end
+
+  create_table "combis", force: :cascade do |t|
+    t.string "patente"
+    t.string "tipo"
+    t.boolean "en_viaje"
+    t.integer "nro_combi"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
