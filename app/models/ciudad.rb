b7785675737358591620acce7441296a867de6ed
Reinoso Francisco <reinosofrancisco_@hotmail.com>
2020-12-02 02:03:55 -0300

    Damas y caballeros. Lo que nunca pidieron, un seed que seedea todo

--- app/models/ciudad.rb
+++ app/models/ciudad.rb
@@ -6,11 +6,13 @@ class Ciudad < ApplicationRecord
     belongs_to :provincia, class_name: 'Provincia'
     validate :ciudad_no_repetida;
     protected
+    
     def ciudad_no_repetida
       if !(Ciudad.where(provincia_id:provincia_id).where(nombre:nombre).empty?)
         errors[:nombre] << 'La ciudad ingresada ya se encuentra cargada'
       end
     end
+
     def destroy
 
         if !(Ruta.where(origen:self).empty? && Ruta.where(destino:self).empty?)
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,39 +18,23 @@
 
 require_relative '../lib/populator_fix.rb'
 
-Ruta.destroy_all
+
 Adicional.destroy_all
 Admin.destroy_all
 User.destroy_all
+Provincia.destroy_all
+Combi.delete_all
+Comentario.destroy_all
+#No puedo usar Destroy
+Viaje.delete_all
+Ciudad.delete_all
+Ruta.delete_all
+Chofer.delete_all
 
 
 
 
-a1=Adicional.find_or_create_by(
-    nombre: "Equipaje extra 5kg",
-    descripcion: "Para poder llevar equipaje extra",
-    precio: 2005.35
-)
-a2=Adicional.find_or_create_by(
-    nombre: "Equipaje extra 10kg",
-    descripcion: "Para poder llevar equipaje extra",
-    precio: 2005.35
-)
-a3=Adicional.find_or_create_by(
-    nombre: "Un alfajor",
-    descripcion: "Adicional para disfrutar un alfajor a bordo de la combi",
-    precio: 2005.35
-)
-a4=Adicional.find_or_create_by(
-    nombre: "Refresco",
-    descripcion: "Adicional para tener un refresco a bordo de la combi",
-    precio: 2005.35
-)
-a5=Adicional.find_or_create_by(
-    nombre: "Recuerdo de viaje",
-    descripcion: "Este adicional provee un recuerdo del destino que hallas escogido(personalizado por cada viaje)",
-    precio: 2005.35
-)
+
 
 #Administrador Generico de prueba
 Admin.create(email: "admin@hotmail.com", password: "admin1234", password_confirmation: "admin1234")
@@ -62,9 +46,29 @@ User.create(
   fecha_nacimiento: Faker::Date.birthday(min_age: 18, max_age: 65),
   dni: rand(45555555 .. 99999999), telefono: Faker::PhoneNumber.phone_number)
 
+#Chofer Generico de prueba
+Chofer.create(
+  email: "admin@hotmail.com", password: "admin1234", password_confirmation: "admin1234",
+  nombre: Faker::Name.first_name, apellido: Faker::Name.last_name,
+  fecha_nacimiento: Faker::Date.birthday(min_age: 18, max_age: 65),
+  dni: rand(45555555 .. 99999999), telefono: Faker::PhoneNumber.phone_number)
 
-#Genero 10 usuarios aleatorios
-User.populate 10 do |u|
+
+
+
+
+
+
+
+#Genero adicionales aleatorios
+Adicional.populate 5 do |u|
+  u.nombre = Faker::Food.dish
+  u.descripcion = Faker::Food.description
+  u.precio = rand(50..500)
+end
+
+#Genero usuarios aleatorios
+User.populate 5 do |u|
   u.nombre = Faker::Name.first_name
   u.apellido = Faker::Name.last_name
   u.fecha_nacimiento = Faker::Date.birthday(min_age: 18, max_age: 65)
@@ -72,4 +76,59 @@ User.populate 10 do |u|
   u.dni = rand(45555555 .. 99999999)
   u.encrypted_password = "KASJDIJASLD"
   u.telefono = Faker::PhoneNumber.phone_number
+end
+
+#Provincias y ciudades
+Provincia.populate 4 do |j|
+  j.nombre = Faker::Address.state
+
+  Ciudad.populate 1 do |k|
+    k.nombre = Faker::Address.city
+    k.provincia_id = j.id
+  end
+
+end
+
+#Genero una ruta de prueba
+Ruta.populate 1 do |j|
+  j.nombre = Faker::Mountain.range
+  j.origen_id = Ciudad.all.sample.id
+  j.destino_id = Ciudad.all.sample.id
+end
+
+#Genero 10 choferes de prueba
+Chofer.populate 4 do |u|
+  u.nombre = Faker::Name.first_name
+  u.apellido = Faker::Name.last_name
+  u.fecha_nacimiento = Faker::Date.birthday(min_age: 18, max_age: 65)
+  u.email = Faker::Internet.email
+  u.dni = rand(45555555 .. 99999999)
+  u.encrypted_password = "KASJDIJASLD"
+  u.telefono = Faker::PhoneNumber.phone_number
+end
+
+#Genero combis de prueba
+Combi.populate 4 do |u|
+  u.cantidad_de_asientos = 12
+  u.patente = Faker::Vehicle.license_plate
+end
+
+
+
+#Genero un viaje de prueba
+Viaje.populate 1 do |j|
+  j.nombre = Faker::Space.nebula
+  j.fecha = Faker::Date.forward(days: 5)
+  j.hora_salida = Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)
+  j.duracion = Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)
+  j.ruta_id = Ruta.all.sample.id
+  j.chofer_id = Chofer.all.sample.id
+  j.combi_id = Combi.all.sample.id
+end
+
+#Genero 10 comentarios de prueba
+Comentario.populate 4 do |u|
+  u.mensaje = Faker::ChuckNorris.fact
+  u.user_id = User.all.sample.id
+  u.viaje_id = Viaje.all.sample.id
 end
