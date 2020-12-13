5ecd821b6a12b47a4affbde3a446059b0c54650a
reinosofrancisco <reinosofrancisco_@hotmail.com>
2020-12-13 00:45:49 -0300

    Botonesios

--- app/controllers/choferes_principal_controller.rb
+++ app/controllers/choferes_principal_controller.rb
@@ -2,6 +2,7 @@ class ChoferesPrincipalController < ApplicationController
   def ver_viaje
     fecha= Date.today
     viaje= Viaje.where((['fecha >= ? AND fecha < ?', fecha, fecha+100])).where(chofer_id:current_chofer).order(:fecha).first
+    @f = User.new
     if(viaje != nil)
       @pasajes= Pasaje.where(viaje_id: viaje.id)
     end
